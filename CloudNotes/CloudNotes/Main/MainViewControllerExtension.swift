26b7e8e63948a8c3dd52e93d9ad81927ef6dcf11
wonhee <wonhee010@gmail.com>
2021-02-16 16:57:46 +0900

    feat: add mainVC extension to tableview

--- CloudNotes/CloudNotes/Main/MainViewControllerExtension.swift
+++ CloudNotes/CloudNotes/Main/MainViewControllerExtension.swift
@@ -5,4 +5,19 @@
 //  Created by Wonhee on 2021/02/16.
 //
 
-import Foundation
+import UIKit
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension MainViewController: UITableViewDelegate {
+}
