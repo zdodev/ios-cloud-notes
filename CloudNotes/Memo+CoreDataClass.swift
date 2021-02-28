e6d6eb513ece8fe180492875ce3744749ec34ac7
wonhee <wonhee010@gmail.com>
2021-02-28 19:09:11 +0900

    feat: add DateExtension

--- CloudNotes/Memo+CoreDataClass.swift
+++ CloudNotes/Memo+CoreDataClass.swift
@@ -11,9 +11,5 @@ import CoreData
 
 @objc(Memo)
 public class Memo: NSManagedObject {
-    func dateToString() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy.MM.dd"
-        return dateFormatter.string(from: self.lastModified ?? Date())
-    }
+    
 }
