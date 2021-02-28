dad182b268d3e923926f60d919d749ae29542af7
wonhee <wonhee010@gmail.com>
2021-02-28 19:09:23 +0900

    feat: add blank method

--- CloudNotes/CloudNotes/Extension/StringExtension.swift
+++ CloudNotes/CloudNotes/Extension/StringExtension.swift
@@ -11,4 +11,14 @@ extension String {
     var isNotEmpty: Bool {
         return !self.isEmpty
     }
+    
+    var isBlank: Bool {
+        get {
+            self.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: "\n", with: "").isEmpty
+        }
+    }
+    
+    var isNotBlank: Bool {
+        return !isBlank
+    }
 }
